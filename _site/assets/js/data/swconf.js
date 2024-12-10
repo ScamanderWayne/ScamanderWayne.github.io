9479d01fc1f5547a3db5d62a1eb4a1b53e866484
ScamanderWayne <scamander.vaine@seznam.cz>
2024-12-10 12:31:03 +0100

    feat(finishing): fuckmylife3.0

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1733830207',resources: [
+      '/ScamanderWayne.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/ScamanderWayne.github.io/',
+      
+        '/ScamanderWayne.github.io/categories/',
+      
+        '/ScamanderWayne.github.io/tags/',
+      
+        '/ScamanderWayne.github.io/archives/',
+      
+        '/ScamanderWayne.github.io/about/',
+      
+        '/ScamanderWayne.github.io/projects/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
